cf76d0bf8749479a652e3089b981bbe49f59efc3
ProfNuru <nurudeen.karim2016@gmail.com>
2022-07-08 19:10:08 +0000

    ready to deploy

---
+++ src/pages/About.tsx
@@ -0,0 +1,6 @@
+export function About(){
+    return <>
+        <h3>Developed by: Nurudeen Abdul-Karim</h3>
+        <a target="_blank" href="https://nourhouse.netlify.app">Visit Portfolio</a>
+    </>
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+export function Home(){
+    return <>
+        <h1>React Shopping Cart with TypeScript and ContextAPI</h1>
+        <h5>Dependencies used:</h5>
+        <ul>
+            <li>React DOM</li>
+            <li>React Router DOM</li>
+            <li>Bootstrap</li>
+        </ul>
+    </>
+}
